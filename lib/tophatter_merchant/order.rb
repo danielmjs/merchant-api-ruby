322a7a5b7a705dcda4820c6a7f35ba5568434b2b
Chris Liu <liu.chris@gmail.com>
2016-03-22 09:57:27 -0700

    Added #all method to Order class.

--- lib/tophatter_merchant/order.rb
+++ lib/tophatter_merchant/order.rb
@@ -5,6 +5,12 @@ module TophatterMerchant
       Hashie::Mash.new get(url: "#{path}/schema.json")
     end
 
+    def self.all(filter: 'all', page: 1, per_page: 50)
+      get(url: "#{path}.json", params: { filter: filter, page: page, per_page: per_page }).map do |hash|
+        Hashie::Mash.new hash
+      end
+    end
+
     protected
 
     def self.path
