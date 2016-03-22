a30d4d86b4d4c64abcc0a77fe7e83bdd1dc2f562
cte <cestreich@gmail.com>
2016-03-22 13:29:23 -0700

    Add an API call for metadata.

--- lib/tophatter_merchant/resource.rb
+++ lib/tophatter_merchant/resource.rb
@@ -22,23 +22,35 @@ module TophatterMerchant
 
       def execute(request)
         begin
-          puts "#{request.method.upcase} #{request.url}"
+          puts "#{request.method.upcase} #{request.url} #{request.payload.inspect}"
           response = request.execute
           raise BadContentTypeException.new, "The server didn't return JSON. You probably made a bad request." if response.headers[:content_type] == 'text/html; charset=utf-8'
           JSON.parse(response.body)
         rescue RestClient::Request::Unauthorized => e
-          raise UnauthorizedException.new, JSON.parse(e.response)['message']
+          error = begin
+            JSON.parse(e.response)
+          rescue
+            {}
+          end
+          raise UnauthorizedException.new, error['message']
         rescue RestClient::BadRequest => e
-          raise BadRequestException.new, JSON.parse(e.response)['message']
+          error = begin
+            JSON.parse(e.response)
+          rescue
+            {}
+          end
+          raise BadRequestException.new, error['message']
         rescue RestClient::ResourceNotFound
-          raise NotFoundException.new, "The API path you requested doesn't exist."
+          raise NotFoundException.new, 'The API path you requested does not exist.'
         rescue RestClient::InternalServerError
-          raise ServerErrorException.new, "The server encountered an internal error. This is probably a bug, and you should contact support."
+          raise ServerErrorException.new, 'The server encountered an internal error. This is probably a bug, and you should contact support.'
         end
       end
 
       def request(method:, url:, params:)
-        RestClient::Request.new(method: method, url: url, payload: params.merge(access_token: TophatterMerchant.access_token), accept: :json)
+        payload = params.merge(access_token: TophatterMerchant.access_token)
+        # raise Exception, payload[:product_variations].inspect
+        RestClient::Request.new(method: method, url: url, payload: payload, accept: :json)
       end
 
       def path
