3b4c49ae238e63f4b9817037ff8ec797d75708d3
cte <cestreich@gmail.com>
2016-04-02 22:17:24 -0700

    Clean up API key resource.

--- lib/tophatter_merchant/api_key.rb
+++ lib/tophatter_merchant/api_key.rb
@@ -5,26 +5,33 @@ module TophatterMerchant
 
     class << self
 
+      # ap TophatterMerchant::ApiKey.schema
       def schema
         get(url: "#{path}/schema.json")
       end
 
+      # ap TophatterMerchant::ApiKey.all.map(&:to_h)
       def all
-        response = get(url: "#{path}.json")
-        response.map { |hash| ApiKey.new(hash) }
+        get(url: "#{path}.json").map do |hash|
+          ApiKey.new(hash)
+        end
       end
 
+      # ap TophatterMerchant::ApiKey.retrieve(1).to_h
       def retrieve(id)
         ApiKey.new get(url: "#{path}/#{id}.json")
       end
 
+      # ap TophatterMerchant::ApiKey.create.to_h
       def create
         ApiKey.new post(url: "#{path}.json")
       end
 
+      # ap TophatterMerchant::ApiKey.destroy(TophatterMerchant::ApiKey.all.last.id).map(&:to_h)
       def destroy(id)
-        response = delete(url: "#{path}/#{id}.json")
-        response.map { |hash| ApiKey.new(hash) }
+        delete(url: "#{path}/#{id}.json").map do |hash|
+          ApiKey.new(hash)
+        end
       end
 
       protected
