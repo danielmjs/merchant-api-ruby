b003e9868fde7123317b81cd91a655bafd650eeb
cte <cestreich@gmail.com>
2016-04-02 22:33:30 -0700

    Clean up image resource.

--- lib/tophatter_merchant/image.rb
+++ lib/tophatter_merchant/image.rb
@@ -1,12 +1,16 @@
 module TophatterMerchant
   class Image < Resource
 
-    attr_accessor :image_id
+    attr_accessor :id, :fingerprint, :url
 
     class << self
 
+      # request = RestClient::Request.new(method: :get, url: 'https://img0.etsystatic.com/101/0/7856452/il_fullxfull.882030160_r0tn.jpg')
+      # response = request.execute
+      # File.open('/tmp/foo.jpg', 'w') { |file| file.write(response.body) }
+      # ap TophatterMerchant::Image.create(File.open('/tmp/foo.jpg')).to_h
       def create(file)
-        Hashie::Mash.new post(url: "#{path}.json", params: { data: file })
+        Image.new post(url: "#{path}.json", params: { data: file })
       end
 
       protected
