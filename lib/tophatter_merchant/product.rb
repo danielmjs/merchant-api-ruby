118698d7152db07cbe5c08e3c7dba788558a6d97
cte <cestreich@gmail.com>
2016-06-17 01:34:27 -0700

    Add weight as a field.

--- lib/tophatter_merchant/product.rb
+++ lib/tophatter_merchant/product.rb
@@ -6,7 +6,7 @@ module TophatterMerchant
     attr_accessor :product_condition, :product_brand, :product_material # Facets
     attr_accessor :product_variations # Variations
     attr_accessor :minimum_bid_amount, :buy_now_price, :retail_price, :cost_basis # Pricing
-    attr_accessor :ships_from, :shipping_price, :expedited_shipping_price, :estimated_days_to_ship, :estimated_days_to_deliver, :expedited_days_to_deliver # Shipping
+    attr_accessor :ships_from, :shipping_price, :expedited_shipping_price, :estimated_days_to_ship, :estimated_days_to_deliver, :expedited_days_to_deliver, :weight # Shipping
     attr_accessor :primary_image, :extra_images, :all_images # Images
     attr_accessor :created_at, :updated_at, :disabled_at, :deleted_at # Timestamps
     attr_accessor :slug
