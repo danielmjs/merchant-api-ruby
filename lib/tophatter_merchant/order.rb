ee54ca1b905c89739feed1d221c8afe6afd49a08
benallen <benallen1@gmail.com>
2016-06-27 19:59:55 -0700

    Enable refunds and standardize routes.

--- lib/tophatter_merchant/order.rb
+++ lib/tophatter_merchant/order.rb
@@ -1,9 +1,10 @@
 module TophatterMerchant
   class Order < Resource
 
-    attr_accessor :id, :product_name, :product_identifier, :variation_identifier
+    attr_accessor :order_id, :product_name, :product_identifier, :variation_identifier
     attr_accessor :customer_name, :address1, :address2, :city, :state, :postal_code, :country
     attr_accessor :carrier, :tracking_number
+    attr_accessor :refund_amount, :available_refunds
 
     class << self
 
@@ -23,12 +24,15 @@ module TophatterMerchant
 
       # ap TophatterMerchant::Order.retrieve(681195262).to_h
       def retrieve(id)
-        Order.new get(url: "#{path}/#{id}/retrieve.json")
+        Order.new get(url: "#{path}/retrieve.json", params: {
+          order_id: id
+        })
       end
 
       # ap TophatterMerchant::Order.fulfill(417953232, carrier: 'other', tracking_number: 'ABC123').to_h
       def fulfill(id, carrier:, tracking_number:)
-        Order.new post(url: "#{path}/#{id}/fulfill.json", params: {
+        Order.new post(url: "#{path}/fulfill.json", params: {
+          order_id: id,
           carrier: carrier,
           tracking_number: tracking_number
         })
@@ -36,6 +40,17 @@ module TophatterMerchant
 
       def unfulfill(id)
         post(url: "#{path}/#{id}/unfulfill.json")
+      end
+
+      # ap TophatterMerchant::Order.refund(417953232, type: 'full', reason: 'delay_in_shipping').to_h
+      def refund(id, type:, reason:, explanation: nil, fees: [])
+        Order.new post(url: "#{path}/refund.json", params: {
+          order_id: id,
+          type: type,
+          reason: reason,
+          explanation: explanation,
+          fees: fees
+        })
       end
 
       protected
